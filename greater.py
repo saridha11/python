4c2f1c77bf6995e5c994376503e6ddd41d591454
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-07-21 17:00:05 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,15 @@
+def main4():
+    a=int(input("num 1"))
+    b=int(input("num 2"))
+    c=int(input("num 3"))
+    if (a>b):
+        if(a>c):
+            print("a is greater")
+        else:
+            print("c is greater")
+    else:
+        if(b>c):
+            print("b is greater")
+        else:
+            print("a is greater")
+main4()
