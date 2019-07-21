3555e80fa6154622f10c00c2cae07bfc9c9e1c86
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-07-21 18:14:50 +0530

    Add files via upload

---
+++ natural.py
@@ -0,0 +1,11 @@
+def main7():
+    n=3
+    if n < 0:
+       print("Enter a positive number")
+    else:
+       sum = 0
+    while(n > 0):
+       sum += n
+       n-= 1
+    print("The sum is",sum)
+main7()
