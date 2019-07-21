ca0ba599114b55cc13a91de85efe7ef2c6ca466d
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-07-21 16:32:43 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,10 @@
+def main2():
+    ch=input()
+    if(ch=="a" or ch=="A" or ch=="e" or ch=="E"or ch=="i" or ch=="I" or ch=="o" or ch=="O" or ch=="u" or ch=="U"):
+        print("vowels")
+    elif(ch>="a" or ch>="A", ch<="z" or ch<="Z"):
+        print("constant")
+    else:
+        print("invalid")
+main2()
+    
