6a1d1c29e86845ddf347afb52abbe546f7010ad0
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-07-22 19:53:08 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,8 @@
+def main():
+    num = input('Enter any number : ')
+    val = int(num)
+    if num == str(num)[::-1]:
+        print('The given number is PALINDROME')
+    else:
+        print('The given number is NOT a palindrome')
+main()
