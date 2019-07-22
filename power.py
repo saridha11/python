45719423b7465f9b72070b00390024163ae3f151
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-07-22 19:36:17 +0530

    Add files via upload

---
+++ power.py
@@ -0,0 +1,7 @@
+import math
+def main():
+    x=int(input("enter the num"))
+    n=int(input("enter the num"))
+    power=math.pow(x,n)
+    print("the result of power {0} Power {1} = {2}".format(x,n, power))
+main()
