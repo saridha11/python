a6d433ccee440c2f70ef66e193346da0989a54d6
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-07-25 19:37:27 +0530

    Add files via upload

---
+++ interval prime.py
@@ -0,0 +1,11 @@
+a= 1
+b = 10
+print("Prime numbers between",a,"and"b,"are:")
+
+for num in range(a,b + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
