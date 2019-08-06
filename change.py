550b6dd0234d8906cebdba693b7bd20825f7aba6
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-08-06 22:45:07 +0530

    Update change.py

--- change.py
+++ change.py
@@ -1,18 +1,18 @@
 def change(s1, s2): 
-    m = len(s1) 
-    n = len(s2) 
-    if abs(m - n) > 1: 
+    l = len(s1) 
+    r = len(s2) 
+    if abs(l - r) > 1: 
         return false
     count = 0
     i = 0
     j = 0
-    while i < m and j < n: 
+    while i < l and j < r: 
         if s1[i] != s2[j]: 
             if count == 1: 
                 return false 
-            if m > n: 
+            if l > r: 
                 i+=1
-            elif m < n: 
+            elif l < r: 
                 j+=1
             else:    
                 i+=1
@@ -22,7 +22,7 @@ def change(s1, s2):
         else:    
             i+=1
             j+=1
-    if i < m or j < n: 
+    if i < l or j < r: 
         count+=1
   
     return count == 1
