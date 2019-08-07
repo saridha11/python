aef6a8fad365c46102d4231f2440f29756171f76
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-08-07 23:18:57 +0530

    Add files via upload

---
+++ 1st repnum.py
@@ -0,0 +1,11 @@
+def countX(l, x): 
+    count = 0
+    n=('7')
+    for ele in lst: 
+        if (ele == x): 
+            count = count + 1
+    return count 
+lst = [1,2,3,2,3,4,3] 
+x = 2
+print('2 times')
+countX(1, x)
