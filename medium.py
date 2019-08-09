1e66b4bf8ad3870aaaec27c1bc9cf96b78bea05e
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-08-09 10:22:09 +0530

    Add files via upload

---
+++ medium.py
@@ -0,0 +1,18 @@
+count=input()
+x = input()
+y = input()
+z = input()
+if y < x and x < z:
+    print(x)
+elif z < x and x < y:
+    print(x)
+    
+elif z < y and y < x:
+    print(y)
+elif x < y and y < z:
+    print(y)
+    
+elif y < z and z < x:
+    print(z)    
+elif x < z and z < y:
+    print(z)
