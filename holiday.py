c5aa5024c26b6e6d69f409d1551690b925f592e7
saridha11 <53110120+saridha11@users.noreply.github.com>
2019-08-10 13:52:06 +0530

    Add files via upload

---
+++ holiday.py
@@ -0,0 +1,9 @@
+n=input()
+days=("sunday","monday","tuesday","wednesday","thursday","friday","saturday")
+if(days=="sunday"):
+    print("yes")
+elif(days=="saturday"):
+    print("yes")
+else:
+    print("no")
+    
