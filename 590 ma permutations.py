82bbab634efec40619f09e9852c550c1a6c41f3a
saridha11 <53110120+saridha11@users.noreply.github.com>
2020-01-06 19:21:13 +0530

    Create 590 ma permutations.py

---
+++ 590 ma permutations.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+s = input()
+L = [ ''.join(x) for x in list(permutations(s,len(s)))]
+L2 = list(set(L))
+print(L2)
+sum1=0
+for x in L2 :
+    sum1 += int(x)
+print(sum1)
