3b9f81fada42d89fa0c9089a8d3e0729f88a4521
saridha11 <53110120+saridha11@users.noreply.github.com>
2022-12-02 13:53:49 +0530

    Update rev num.py

--- rev num.py
+++ rev num.py
@@ -1,9 +1,11 @@
-def rev():
-    n=423
-    rev=0
-    while(n>0):
-        rem=n%10
-        rev=(rev*10)+rem
-        n=n//10
-        print("the rev is:",rev)
-rev()
+a = (input())
+reverse = 0
+try:
+    val = int(a)
+    while val > 0:
+        reminder = val % 10
+        reverse = (reverse * 10) + reminder
+        val //= 10
+    print(reverse)
+except ValueError:
+    print("not valid")
